a5c7477ae58e63ac491745df569a687d7e74b55d
Gavin Luo <luoxiangyu528@gmail.com>
2019-05-16 23:40:50 -0400

    SLerp rotation for camera.

--- Assets/Scripts/CameraRig.cs
+++ Assets/Scripts/CameraRig.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 public class CameraRig : MonoBehaviour
 {
 
-    [SerializeField] GameObject m_attachObject;
-    [SerializeField] float m_rotationSpeed = 10.0f;
-    [SerializeField] float m_cameraDistance = 10.0f;
+    [SerializeField] GameObject attachObject;
+    [SerializeField] float rotationSpeed = 10.0f;
+    [SerializeField] float cameraDistance = 10.0f;
+    [SerializeField][Range(0f,1f)] float rotationSmoothness = 0.5f;
 
     private Vector3 rotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = m_attachObject.transform.position;
+        transform.position = attachObject.transform.position;
         transform.rotation = Quaternion.identity;
 
         var cameraChildren = GetComponentInChildren<Camera>()?.gameObject;
         if (cameraChildren != null)
         {
             cameraChildren.transform.rotation = Quaternion.identity;
-            cameraChildren.transform.position = transform.position + new Vector3(0, 0, -m_cameraDistance);
+            cameraChildren.transform.position = transform.position + new Vector3(0, 0, -cameraDistance);
         }
 
         rotation = transform.rotation.eulerAngles;
@@ -30,16 +31,16 @@ public class CameraRig : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = m_attachObject.transform.position;
+        transform.position = attachObject.transform.position;
 
         float x = Input.GetAxis("Mouse X");
         float y = Input.GetAxis("Mouse Y");
 
-        rotation.x -= y * m_rotationSpeed;
+        rotation.x -= y * rotationSpeed;
 
         rotation.x = Mathf.Clamp(rotation.x, -90, 90);
-        rotation.y += x * m_rotationSpeed;
+        rotation.y += x * rotationSpeed;
 
-        transform.rotation = Quaternion.Euler(rotation);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotation), 0.5f);
     }
 }
