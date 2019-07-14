6470277630d34ee919004b4a03199e041a1a92d8
Gavin Luo <luoxiangyu528@gmail.com>
2019-07-14 08:26:58 -0400

    Update stick-figure to fbx

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,31 +9,102 @@ public class CharacterMovement : MonoBehaviour
 
     [SerializeField] CameraRig m_cameraRig;
     [SerializeField] float m_topSpeed = 5.0f;
-    [SerializeField] float acceleration = 10.0f;
+    [SerializeField] float m_acceleration = 10.0f;
     [SerializeField] [Range(0f, 1f)] float playerRotationSmoothness = 0.5f;
-    
+    [SerializeField] float m_jumpHeight = 20.0f;
+    [SerializeField] float m_jumpDelay = 0.3f;
+
     Animator m_animator;
-    CharacterController m_characterController;
+    Rigidbody m_rigidBody;
     float m_currentSpeed = 0.0f;
+    bool m_isInAir = false;
+    bool m_isAttacking = false;
 
     // Start is called before the first frame update
     void Start()
     {
         m_animator = GetComponent<Animator>();
-        m_characterController = GetComponent<CharacterController>();
+        m_rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Move();
+        Jump();
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    public void BeginAttacking()
+    {
+        m_isAttacking = true;
+    }
+
+    public void EndAttacking()
+    {
+        m_isAttacking = false;
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            m_animator.SetTrigger("attack");
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && !m_isInAir)
+        {
+            InstantJump();
+        }
+    }
+
+    private IEnumerator DelayJumping()
+    {
+        m_animator.SetTrigger("jump");
+        yield return new WaitForSeconds(m_jumpDelay);
+        var currentVelocity = m_rigidBody.velocity;
+        currentVelocity.y = m_jumpHeight;
+        m_rigidBody.velocity = currentVelocity;
+    }
+
+    private void InstantJump()
+    {
+        m_animator.SetTrigger("jump");
+        var currentVelocity = m_rigidBody.velocity;
+        currentVelocity.y = m_jumpHeight;
+        m_rigidBody.velocity = currentVelocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
+        {
+            m_isInAir = false;
+            m_animator.SetBool("inAir", m_isInAir);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
+        {
+            m_isInAir = true;
+            m_animator.SetBool("inAir", m_isInAir);
+        }
     }
 
     private void Move()
     {
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        if (direction.magnitude >= BIG_EPSILON)
+        if (direction.magnitude >= BIG_EPSILON && !m_isAttacking)
         {
             direction.Normalize();
             var cameraRotation = m_cameraRig.transform.eulerAngles;
@@ -42,11 +114,11 @@ public class CharacterMovement : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, rotateTo, playerRotationSmoothness);
             if (m_currentSpeed < m_topSpeed)
             {
-                m_currentSpeed += acceleration * Time.deltaTime;
+                m_currentSpeed += m_acceleration * Time.deltaTime;
             }
             var vectorSpeed = direction * m_currentSpeed;
-            vectorSpeed.y = m_characterController.velocity.y;
-            m_characterController.SimpleMove(vectorSpeed);
+            vectorSpeed.y = m_rigidBody.velocity.y;
+            m_rigidBody.velocity = vectorSpeed;
 
             m_animator.SetFloat("speedPercent", m_currentSpeed / m_topSpeed);
         }
@@ -54,13 +126,13 @@ public class CharacterMovement : MonoBehaviour
         {
             if (m_currentSpeed > 0.0f)
             {
-                m_currentSpeed -= 2 * acceleration * Time.deltaTime;
+                m_currentSpeed -= 2 * m_acceleration * Time.deltaTime;
                 if (m_currentSpeed < 0.0f)
                 {
                     m_currentSpeed = 0.0f;
                 }
                 var playerDirection = transform.forward * m_currentSpeed;
-                m_characterController.SimpleMove(new Vector3(playerDirection.x, m_characterController.velocity.y, playerDirection.z));
+                m_rigidBody.velocity = new Vector3(playerDirection.x, m_rigidBody.velocity.y, playerDirection.z);
             }
             else
             {
